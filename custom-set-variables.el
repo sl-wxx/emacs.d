573b01263a6d1b2b1044b3355bcf17733ea5fcbe
刘帅 <liu.shuai@hejinonline.com>
2018-05-02 18:40:25 +0800

    添加customer-set-variables.el和my/.customer.el

---
+++ custom-set-variables.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(git-gutter:handled-backends (quote (svn hg git)))
+ '(jdee-server-dir "/home/lisp/src/jdee-emacs/jdee-server")
+ '(org-src-tab-acts-natively t)
+ '(session-use-package t nil (session))
+ '(w3m-default-display-inline-images t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(window-numbering-face ((t (:foreground "DeepPink" :underline "DeepPink" :weight bold))) t))
